b135d016a34ee73e6678ac2e421fcdeefc01ba58
WMXPY <wm@mengw.io>
2020-04-22 11:25:51 -0500

    implement range function

--- src/calculate/range.ts
+++ src/calculate/range.ts
@@ -4,13 +4,9 @@
  * @description Range
  */
 
-import { Coordinate, CoordinateRange, createCoordinate, GetCoordinateFunction } from "../declare/declare";
+import { Coordinate, CoordinateRange, createCoordinate, createInfinityRange, GetCoordinateFunction } from "../declare/declare";
 
-export const getLinearCenterByCoordinates = (coordinates: Coordinate[]): Coordinate | null => {
-
-    if (coordinates.length === 0) {
-        return null;
-    }
+export const getLinearRangeByCoordinates = (coordinates: Coordinate[]): CoordinateRange => {
 
     const reduced: CoordinateRange = coordinates.reduce<CoordinateRange>(
         (previous: CoordinateRange, current: Coordinate): CoordinateRange => {
@@ -21,28 +17,17 @@ export const getLinearCenterByCoordinates = (coordinates: Coordinate[]): Coordin
                 maxLongitude: Math.max(previous.maxLongitude, current.longitude),
                 minLongitude: Math.min(previous.minLongitude, current.longitude),
             };
-        }, {
-            maxLatitude: -Infinity,
-            minLatitude: Infinity,
-            maxLongitude: -Infinity,
-            minLongitude: Infinity,
-        } as CoordinateRange,
+        },
+        createInfinityRange(),
     );
 
-    const finalLatitude: number = (reduced.maxLatitude + reduced.minLatitude) / 2;
-    const finalLongitude: number = (reduced.maxLongitude + reduced.minLongitude) / 2;
-
-    return createCoordinate(finalLatitude, finalLongitude);
+    return reduced;
 };
 
 export const getLinearCenterByObjects = <T extends any>(
     objects: T[],
     getCoordinateFunction: GetCoordinateFunction<T>,
-): Coordinate | null => {
-
-    if (objects.length === 0) {
-        return null;
-    }
+): CoordinateRange => {
 
     const reduced: CoordinateRange = objects.reduce<CoordinateRange>(
         (previous: CoordinateRange, current: T): CoordinateRange => {
@@ -55,70 +40,9 @@ export const getLinearCenterByObjects = <T extends any>(
                 maxLongitude: Math.max(previous.maxLongitude, coordinate.longitude),
                 minLongitude: Math.min(previous.minLongitude, coordinate.longitude),
             };
-        }, {
-            maxLatitude: -Infinity,
-            minLatitude: Infinity,
-            maxLongitude: -Infinity,
-            minLongitude: Infinity,
-        } as CoordinateRange,
+        },
+        createInfinityRange(),
     );
 
-    const finalLatitude: number = (reduced.maxLatitude + reduced.minLatitude) / 2;
-    const finalLongitude: number = (reduced.maxLongitude + reduced.minLongitude) / 2;
-
-    return createCoordinate(finalLatitude, finalLongitude);
-};
-
-export const getWeightedCenterByCoordinates = (coordinates: Coordinate[]): Coordinate | null => {
-
-    if (coordinates.length === 0) {
-        return null;
-    }
-
-    const reduced: Coordinate = coordinates.reduce<Coordinate>(
-        (previous: Coordinate, current: Coordinate): Coordinate => {
-
-            return {
-                latitude: previous.latitude + current.latitude,
-                longitude: previous.longitude + current.longitude,
-            };
-        }, {
-            latitude: 0,
-            longitude: 0,
-        } as Coordinate,
-    );
-
-    const finalLatitude: number = reduced.latitude / coordinates.length;
-    const finalLongitude: number = reduced.longitude / coordinates.length;
-
-    return createCoordinate(finalLatitude, finalLongitude);
-};
-
-export const getWeightedCenterByObjects = <T extends any>(
-    objects: T[],
-    getCoordinateFunction: GetCoordinateFunction<T>,
-): Coordinate | null => {
-
-    if (objects.length === 0) {
-        return null;
-    }
-
-    const reduced: Coordinate = objects.reduce<Coordinate>(
-        (previous: Coordinate, current: T): Coordinate => {
-
-            const coordinate: Coordinate = getCoordinateFunction(current);
-            return {
-                latitude: previous.latitude + coordinate.latitude,
-                longitude: previous.longitude + coordinate.longitude,
-            };
-        }, {
-            latitude: 0,
-            longitude: 0,
-        } as Coordinate,
-    );
-
-    const finalLatitude: number = reduced.latitude / objects.length;
-    const finalLongitude: number = reduced.longitude / objects.length;
-
-    return createCoordinate(finalLatitude, finalLongitude);
+    return reduced;
 };
