7335a5804cec3936241b8c88b9c2af1ef03eda0a
WMXPY <wm@mengw.io>
2020-05-29 10:23:44 -0500

    add random move, improve random garble

--- src/garble/random.ts
+++ src/garble/random.ts
@@ -3,3 +3,16 @@
  * @namespace Geometry_Garble
  * @description Random
  */
+
+import { Coordinate } from "../declare/declare";
+
+export const randomGarbleLocation = (value: number, range: number): number => {
+
+    const diffrence: number = Math.random() * range;
+    return value;
+};
+
+export const randomGarbleCoordinate = (original: Coordinate): Coordinate => {
+
+    return original;
+};
--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export * from "./calculate/range";
 export * from "./declare/convert";
 export * from "./declare/declare";
 export * from "./declare/polygon";
+export * from "./garble/random";
 export * from "./geo-json/multi-polygon";
 export * from "./geo-json/point";
 export * from "./geo-json/polygon";
